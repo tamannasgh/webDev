54444d5a56cccd59307ab8f90226f5ca39d1afe6
Tamanna Sharma <ts.harma2882006@gmail.com>
2022-06-16 20:53:58 +0530

    see yourself

---
+++ css-concepts/transition/ex1/script.js
@@ -0,0 +1,4 @@
+document.getElementById("btn").addEventListener("click", ()=>{
+    document.getElementsByClassName("left")[0].classList.add("animate")
+    document.getElementsByClassName("right")[0].classList.add("animate")
+})
